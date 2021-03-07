e1e0723ca07e607218856c5d456c05d814890117
wgs <wgshuaiit@163.com>
2021-03-07 18:19:18 +0800

    使用springboot整合redisson

--- redisson-lock/src/main/java/com/example/redissonlock/controller/RedissonLockController.java
+++ redisson-lock/src/main/java/com/example/redissonlock/controller/RedissonLockController.java
@@ -15,8 +15,28 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class RedissonLockController {
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     @RequestMapping("redissonLock")
     public String redissonLock() {
+        RLock rLock = redissonClient.getLock("order");
+        try {
+            rLock.lock(30, TimeUnit.SECONDS);
+            log.info("我获得了锁！！！");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log.info("我释放了锁！！");
+            rLock.unlock();
+        }
+        log.info("方法执行完成！！");
+        return "方法执行完成！！";
+    }
+
+    @RequestMapping("redissonLock1")
+    public String redissonLock1() {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://127.0.0.1:6379");
         config.useSingleServer().setPassword("123456");
